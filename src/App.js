9fb7f50286b53c52f0d2a1651d0f7d8650858a6c
Esben <71466904+ebberx@users.noreply.github.com>
2023-04-25 13:06:26 +0200

    Added background to the site

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 // App.js
 import React from 'react';
 import AppRoutes from './Routes.js';
+import background from "./img/background.jpeg";
 
 const App = () => {
     return (
-        <div>
+        <div style={{
+            backgroundImage: `url(${background})`,
+            backgroundPosition: 'center',
+            backgroundSize: 'cover',
+            backgroundRepeat: 'no-repeat',
+            width: '100vw',
+            height: '100vh',
+        }}>
             <AppRoutes />
         </div>
     );
